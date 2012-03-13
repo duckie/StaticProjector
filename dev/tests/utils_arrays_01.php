a28af01cca9a12a7477c81c9888c3ec675448a73
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2012-03-13 18:29:20 +0100

    New test on the default config file.

--- dev/tests/utils_arrays_01.php
+++ dev/tests/utils_arrays_01.php
@@ -27,6 +27,10 @@ class sp_utils_arrays_01 extends sp_test
 		sp_ArrayUtils::store_config($array2, $file_test);
 		$array_result = sp_ArrayUtils::load_config($file_test);
 		$success = $success && ($array_result == $array2);
+		
+		$file_test = $this -> create_ref_to_check();
+		$array_config = sp_ArrayUtils::load_config($this -> root_dir()."/sp-includes/defaults/".sp_StaticProjector::config_file);
+		sp_ArrayUtils::store_config($array_config, $file_test);
 
 		return $success;
 	}
